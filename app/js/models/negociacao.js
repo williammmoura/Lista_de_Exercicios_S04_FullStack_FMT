c025ca1f25af01aafc8017cafe1397fe937e9d65
William Monte de Moura <williamonte@gmail.com>
2023-03-18 16:15:59 -0300

    Exercício 03

---
+++ app/js/models/negociacao.js
@@ -0,0 +1,12 @@
+class Negociação{
+    //Encapsulamento atributos privados
+    #date;
+    #quantidade;
+    #valor;
+    constructor(date, quantidade, valor){
+        //Estanciando os atributos
+        this.#date;
+        this.#quantidade;
+        this.valor;
+    };
+};
