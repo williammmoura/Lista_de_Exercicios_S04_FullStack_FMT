fa6f0df6958351a385cb38fba51bd80c027bf147
William Monte de Moura <williamonte@gmail.com>
2023-03-18 21:37:53 -0300

    Exercício 05

--- app/js/controllers/negociacaoController.js
+++ app/js/controllers/negociacaoController.js
@@ -3,10 +3,23 @@ class NegociacaoController{
     #campoData;
     #campoQuantidade;
     #campoValor;
+    #dadosCliente = new this.#dadosCliente();
 
     constructor(){
         this.#campoData = document.getElementById("data");
         this.#campoQuantidade = document.getElementById("quantidade");
         this.#campoValor = document.getElementById("valor");
+    };
+
+    //Criando um Método (criarNegociacao())
+    criarNegociacao(){
+        let data = this.#campoData.value;
+        let quantidade = this.#campoQuantidade.value;
+        let valor = this.#campoValor.value;
+
+        //Criando uma negociação com os dados do formulário e exibir a negociação criada no console do navegador.
+        let cliente = new Cliente(data, quantidade, valor);
+        this.#dadosCliente.adicionar(cliente);
+        console.log(this.#dadosCliente.cliente);
     };
 };
